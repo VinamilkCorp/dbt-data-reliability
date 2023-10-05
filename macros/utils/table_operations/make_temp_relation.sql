480a56812db309bdf76e2ceb17094deb12c4f18c
cong.dang <dvcong@vinamilk.com.vn>
2023-10-05 18:32:13 +0700

    minor

--- macros/utils/table_operations/make_temp_relation.sql
+++ macros/utils/table_operations/make_temp_relation.sql
@@ -20,7 +20,7 @@
     {% do return(dbt.make_temp_relation(base_relation, suffix)) %}
 {% endmacro %}
 
-{% macro databricks__edr_make_temp_relation(base_relation, suffix) %}
+{% macro clickhouse__edr_make_temp_relation(base_relation, suffix) %}
     {% set tmp_identifier = base_relation.identifier ~ suffix %}
     {% set tmp_relation = base_relation.incorporate(
                                 path={"identifier": tmp_identifier, "schema": base_relation.schema}) -%}
