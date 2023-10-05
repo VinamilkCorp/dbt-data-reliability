aaf9d494a8ed29ee9dc67eddbdf6f888aba0fb06
cong.dang <dvcong@vinamilk.com.vn>
2023-10-05 16:28:14 +0700

    fix datediff

--- macros/utils/cross_db_utils/datediff.sql
+++ macros/utils/cross_db_utils/datediff.sql
@@ -27,14 +27,8 @@
 {% endmacro %}
 
 {% macro clickhouse__edr_datediff(first_date, second_date, date_part) %}
-    {%- if date_part | lower in ['second', 'minute', 'hour', 'day'] %}
-        timestamp_diff('{{ second_date }}', {{ first_date }}, {{ date_part }})
-    {%- elif date_part | lower in ['week', 'month', 'quarter', 'year'] %}
-        {% set macro = dbt.datediff or dbt_utils.datediff %}
-        {% if not macro %}
-            {{ exceptions.raise_compiler_error("Did not find a `datediff` macro.") }}
-        {% endif %}
-        {{ return(macro(date_part, elementary.edr_cast_as_date(first_date), elementary.edr_cast_as_date(second_date))) }}
+    {%- if date_part | lower in ['second', 'minute', 'hour', 'day', 'week', 'month', 'quarter', 'year'] %}
+        timestamp_diff('{{ date_part }}', {{ first_date }}, {{ second_date }})
     {%- else %}
         {{ exceptions.raise_compiler_error("Unsupported date_part in edr_datediff: ".format(date_part)) }}
     {%- endif %}
